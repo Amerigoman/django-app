6ce5ddc9004696cf7022b8bee399c7a701cbe04b
Aleksandr <oleksandrdubrovskyi@gmail.com>
2017-01-07 22:13:29 +0200

    add user authentication

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Location (models.Model):
+    user = models.ForeignKey(User)
     name = models.CharField(max_length=50)
     predators = models.CharField(max_length=100)
     num_restaurants = models.IntegerField()
