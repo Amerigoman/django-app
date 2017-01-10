dc48523a7fab2f87d70e6cb3096b4e9448d2ccfb
Aleksandr <oleksandrdubrovskyi@gmail.com>
2017-01-10 10:07:37 +0200

    ready for deployment

--- Treasuregram/settings.py
+++ Treasuregram/settings.py
@@ -125,8 +125,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    BASE_DIR + '/www/static',
-    BASE_DIR + '/www/media',
+    BASE_DIR + '/main_app/static',
+    BASE_DIR + '/main_app/media',
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
